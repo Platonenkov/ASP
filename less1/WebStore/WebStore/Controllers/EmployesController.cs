ce789b1ca6dd90109dd432a425a8dd2f2c4c6196
Platonenkov <Platonenkov87@gmail.com>
2019-03-30 00:09:25 +0300

    Самодеятельность, разметка и прочее

--- less1/WebStore/WebStore/Controllers/EmployesController.cs
+++ less1/WebStore/WebStore/Controllers/EmployesController.cs
@@ -12,9 +12,9 @@ namespace WebStore.Controllers
 
         private static List<Employee> __Employees = new List<Employee>
         {
-            new Employee {Id=1, SurName="Иванов", FirstName="Иван", Patronymic="Иванович", Age=25 },
-            new Employee {Id=2, SurName="Петров", FirstName="Пётр", Patronymic="Петрович", Age=25 },
-            new Employee {Id=3, SurName="Сидоров", FirstName="Сидор", Patronymic="Сидорович", Age=25 }
+            new Employee {Id=1, SurName="Иванов", FirstName="Иван", Patronymic="Иванович", Age=25, Date= new DateTime(2007, 7, 7) },
+            new Employee {Id=2, SurName="Петров", FirstName="Пётр", Patronymic="Петрович", Age=30, Date= new DateTime(2008, 8, 8) },
+            new Employee {Id=3, SurName="Сидоров", FirstName="Сидор", Patronymic="Сидорович", Age=33, Date= new DateTime(2009, 9, 9) }
         };
 
 
--- less1/WebStore/WebStore/Models/Employee.cs
+++ less1/WebStore/WebStore/Models/Employee.cs
@@ -17,5 +17,7 @@ namespace WebStore.Models
         public string SurName { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
+
+        public DateTime Date { get; set; }
     }
 }
