0dffdba2dfc6eb867a609447b7aeda4fe3c2d5df
Platonenkov <Platonenkov87@gmail.com>
2019-03-29 23:18:35 +0300

    Конец урока

---
+++ less1/WebStore/WebStore/Controllers/EmployesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployesController : Controller
+    {
+
+        private static List<Employee> __Employees = new List<Employee>
+        {
+            new Employee {Id=1, SurName="Иванов", FirstName="Иван", Patronymic="Иванович", Age=25 },
+            new Employee {Id=2, SurName="Петров", FirstName="Пётр", Patronymic="Петрович", Age=25 },
+            new Employee {Id=3, SurName="Сидоров", FirstName="Сидор", Patronymic="Сидорович", Age=25 }
+        };
+
+
+        public IActionResult Index()
+        {
+            return View(__Employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var employe = __Employees.FirstOrDefault(e => e.Id == id);
+            if (employe == null) return NotFound();
+
+            return View(employe);
+        }
+    }
+}
---
+++ less1/WebStore/WebStore/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        /// <summary>
+        /// Id сотрудника
+        /// </summary>
+        public int Id { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+    }
+}
