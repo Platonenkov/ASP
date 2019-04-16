ca607b3e94e7517af6c8018901c70ffe3af85749
Platonenkov <Platonenkov87@gmail.com>
2019-04-16 19:05:58 +0300

    Система Identifity

---
+++ less1/WebStore/WebStore/Components/UserInfoViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Components
+{
+    [ViewComponent(Name ="UserInfo")] //имя компоненту дать тут
+    public class UserInfoViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            if (User.Identity.IsAuthenticated)
+                return View("UserInfoView");
+            return View();
+        }
+    }
+}
---
+++ less1/WebStore/WebStore/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Пароль"), Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
